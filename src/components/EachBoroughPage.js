dbcda2a31e705143d269b4537d2d81b2060fb7cb
mon33k <moniquemojica@ac.c4q.nyc>
2018-03-03 21:42:27 -0500

    added EachBorough.js and Home.js page new routes

---
+++ src/components/EachBoroughPage.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Switch, Link, Route } from 'react-router-dom';
+import axios from 'axios';
+import MapContainer, { BOROUGHS } from '../api/googleMapsAPI';
+
+class EachBoroughPage extends Component {
+
+    constructor(props) {
+        super();
+        const { allAddress } = props
+        this.state = {
+            info: "",
+            list: []
+        }
+        console.log("props bitches", props)
+    }
+
+    // renderList = () => {
+
+    // }
+
+
+    render() {
+        return (
+            <div>
+                Helloooo
+            <MapContainer zoom={10} initialCenter={BOROUGHS.MANHATTAN} />
+            </div>
+        )
+    }
+
+}
+export default EachBoroughPage;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,126 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Switch, Link, Route, Redirect } from 'react-router-dom';
+import EachBoroughPage from './EachBoroughPage';
 
-const Home = () => (
-  <div>
-    <h2>Show me stuff</h2>
-  </div>
-);
+class Home extends Component {
+
+    constructor(props) {
+        super();
+        const { allAddress } = props
+        this.state = {
+            services: ["JobListings", "GedListings", "Other"],
+            boroughs: ["Queens", "Manhattan", "Bronx", "Brooklyn", "StatenIsland"],
+            fireRedirect: false
+        }
+        console.log("allAddresssss", props)
+    }
+
+    handleCheckboxChange = e => {
+        const { boroughs } = this.state
+        boroughs.map((borough) => {
+            if (borough === e.target.name) {
+                this.setState({
+                    borough: e.target.name
+                })
+                console.log("e.target.name", e.target.name)
+            }
+        })
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({
+            fireRedirect: true //only updating the state when the form is submitted
+        })
+    };
+
+    render() {
+        const { fireRedirect } = this.state
+        return (
+            <div>
+                <form onSubmit={this.submitForm}>
+                    <h2>Help Me Find</h2>
+
+                    <p>
+                        GED Locations {" "}
+                        <input
+                            name="GedListings"
+                            type="checkbox"
+                            onChange={this.handleCheckboxChange}
+                        />
+                    </p>
+                    <p>
+                        Financial Assistance Locations {" "}
+                        <input
+                            name="JobListings"
+                            type="checkbox"
+                            onChange={this.handleCheckboxChange}
+                        />
+                    </p>
+                    <p>
+                        Other {" "}
+                        <input
+                            name="Other"
+                            type="checkbox"
+                            onChange={this.handleCheckboxChange}
+                        />
+                    </p>
+
+                    <br />
+
+                    <h3> Located In </h3>
+                    <p>
+                        Queens {" "}
+                        <input
+                            name="Queens"
+                            type="checkbox"
+                            onChange={this.handleCheckboxChange}
+                        />
+                    </p>
+                    <p>
+                        Manhattan {" "}
+                        <input
+                            name="Manhattan"
+                            type="checkbox"
+                            onChange={this.handleCheckboxChange}
+                        />
+                    </p>
+                    <p>
+                        Bronx {" "}
+                        <input
+                            name="Bronx"
+                            type="checkbox"
+                            onChange={this.handleCheckboxChange}
+                        />
+                    </p>
+                    <p>
+                        Brooklyn {" "}
+                        <input
+                            name="Brooklyn"
+                            type="checkbox"
+                            onChange={this.handleCheckboxChange}
+                        />
+                    </p>
+                    <p>
+                        Staten Island {" "}
+                        <input
+                            name="StatenIsland"
+                            type="checkbox"
+                            onChange={this.handleCheckboxChange}
+                        />
+                    </p>
+                    <button type="submit">Submit</button>
+                </form>
+                {fireRedirect && (
+                    <Switch>
+                    <Redirect from='/' to='/byborough'/>
+                    <Route path='/byborough' component={EachBoroughPage}/>
+                  </Switch>
+                )}
+                
+            </div>
+        )
+    }
+};
 
 export default Home;
