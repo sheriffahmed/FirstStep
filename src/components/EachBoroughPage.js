8057d225a348d566c11fef02d213f29f51cccfe0
mon33k <moniquemojica@ac.c4q.nyc>
2018-03-04 19:25:15 -0500

    Changed EachBoroughPage.js into a funcitonal component that Sheriff will pass the api data as props to

--- src/components/EachBoroughPage.js
+++ src/components/EachBoroughPage.js
@@ -3,34 +3,21 @@ import { Switch, Link, Route } from 'react-router-dom';
 import axios from 'axios';
 import MapContainer, { BOROUGHS } from '../api/googleMapsAPI';
 
-class EachBoroughPage extends Component {
-
-    constructor(props) {
-        super();
-        const { allAddress } = props
-        this.state = {
-            info: "",
-            list: []
-        }
-        console.log("props ", props)
-    }
-
-    // renderList = () => {
-    
-
-    // }
+const EachBoroughPage = ({ lat, long, input, }) => {
 
 
-    render() {
-        const testLocations = [{name: "Resource Center 1", latitude: "40.735681", longitude: "-73.988713"}, {name: "Resource Center 2", latitude: "40.808451", longitude: "-73.947112"}]
 
-        return (
-            <div>
+    const testLocations = [{ name: "Resource Center 1", latitude: "40.735681", longitude: "-73.988713" }, { name: "Resource Center 2", latitude: "40.808451", longitude: "-73.947112" }]
+
+    return (
+        <div>
             Helloooo
             <MapContainer zoom={10} initialCenter={BOROUGHS.MANHATTAN} locations={testLocations} />
-            </div>
-        )
-    }
+        </div>
 
+    )
 }
+
+
+
 export default EachBoroughPage;
