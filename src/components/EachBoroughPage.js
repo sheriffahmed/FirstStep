b65f44f5304fa869a8b97b9238b44ab755194d45
sheriffahmed <sheriff.v.ahmed@gmail.com>
2018-03-05 00:03:30 -0500

    son, we got it

--- src/components/EachBoroughPage.js
+++ src/components/EachBoroughPage.js
@@ -9,6 +9,7 @@ class EachBoroughPage extends Component {
         super(props);
         const { allAddress, listing } = this.props
 this.list = [...this.props.listing]
+this.locations = []
 
         this.state = {
             info: "",
@@ -29,17 +30,20 @@ this.list = [...this.props.listing]
 componentDidMount(){
     this.setState({
         listing: [...this.props.listing]
-    })
+    })  
+    console.log("this.props.listings", this.props.listing)
+    this.props.listing.map(place =>{
+            if(place.address && place.latitude && place.longitude){
+            this.locations.push({name: place.address, latitude: place.latitude, longitude: place.longitude }) }
+            console.log(`NewLOCATIONS`, this.locations)
+        })
 }
 
     render() {
         
         const testLocations2 = [{name: "Resource Center 1", latitude: "40.735681", longitude: "-73.988713"}, {name: "Resource Center 2", latitude: "40.808451", longitude: "-73.947112"}]
         const testLocations = []
-        this.props.listing.map(place =>{
-            if(place.address && place.latitude && place.longitude){
-            testLocations2.push({name: place.address, latitude: parse(place.latitude, longitude: place.longitude }) }
-        })
+      
 // let list = [...this.props.listing]
         return (
             <div>
@@ -49,14 +53,14 @@ componentDidMount(){
               <div>
 
                 <li> {place.borough}</li>
-                <li>{place.address}</li>
+                <li>{place.address || place.street_address}</li>
                 
                 </div>
                 )
                
             ) }
             </ul>
-            <MapContainer zoom={10} initialCenter={BOROUGHS.MANHATTAN} locations={testLocations} />
+            <MapContainer zoom={10} initialCenter={BOROUGHS.MANHATTAN} locations={this.locations} />
             </div>
         )
     }
